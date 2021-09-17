44707dad7c601f99b219d7370cca6702580c2ae9
Macheing <42636064+Macheing@users.noreply.github.com>
2021-09-17 13:03:59 +0800

    simple and funny madlibs game

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+
+# string mad libs
+
+adj = input('Enter an adjective here: ')
+verb1 = input('Enter a verb here: ')
+verb2 = input('Enter a verb here: ')
+famous_person = input("Enter your celibrity's name here: ")
+madlibs = f"Hacking is so {adj}!, it makes me so excited all the time, because I love to {verb1}.\
+ Stay motivated and {verb2} like you are {famous_person}"
+print(madlibs)

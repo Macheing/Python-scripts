801e5bbcaac482d5cb59a0b132efda8a8a2b5da6
Macheing <42636064+Macheing@users.noreply.github.com>
2021-10-03 22:10:28 +0800

    late night commit

--- factorial.py
+++ factorial.py
@@ -1,11 +1,21 @@
 # return factorial number of given number
-def factorial(n):
+# using for loop 
+def loop_factorial(num):
     result = 1
-    for i in range(1,n):
+    for i in range(1,num+1):
         result = result*i  
-        return result
+    return result
+
+# recursive factorial
+def recursive_factorial(num):
+    if num < 2:
+        return 1
+    return num * recursive_factorial(num-1)
+
+print('For loop factorial vs Recursive factorial')
+for num in range(100):
+    print('Factorial of:',num, loop_factorial(num),'<== vs ==>',
+              recursive_factorial(num))
     
-# computing factorial consumes more memory like recursive functions
-for n in range(0,10):
-    print(n, factorial(n+1))
+    
     
